1ab815a770c7c3e88714800d2202c79c2cd070d4
Adewale Adekola <your-github-email>
2026-09-09 18:58:56 +0100

    Update message to test auto-trigger

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="Hello, my name is Adewale Adekola")
+    return jsonify(message="Hello, my name is Adewale Adekola - CI/CD is working!")
 
 @app.route("/health")
 def health():
